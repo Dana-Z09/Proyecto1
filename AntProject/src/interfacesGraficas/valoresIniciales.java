d1054617cf4e472fa6afff91bc122e5f4fbf045b
Daniela Zambrano <d.zambrano@correo.unimet.edu.ve>
2024-02-18 11:30:13 -0400

    Creacion de jFrames principales
    firstPage
    menu
    creditos
    newGrafo
    uploadGrafo
    valoresIniciales
    editGrafo

--- AntProject/src/interfacesGraficas/valoresIniciales.java
+++ AntProject/src/interfacesGraficas/valoresIniciales.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package Interfaces;
+package interfacesGraficas;
 
 /**
  *
  * @author Danna Star
  */
-public class Bienvenida extends javax.swing.JFrame {
+public class valoresIniciales extends javax.swing.JFrame {
 
     /**
-     * Creates new form Bienvenida
+     * Creates new form valoresIniciales
      */
-    public Bienvenida() {
+    public valoresIniciales() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class Bienvenida extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Bienvenida.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(valoresIniciales.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Bienvenida.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(valoresIniciales.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Bienvenida.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(valoresIniciales.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Bienvenida.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(valoresIniciales.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Bienvenida().setVisible(true);
+                new valoresIniciales().setVisible(true);
             }
         });
     }
